db55fbb64c935279a888d220ac13d2736a693565
Ivan Gavrilov <gavrilovivan564@gmail.com>
2020-01-28 19:02:15 +0200

    start

---
+++ src/main/java/com/projectjb/couponsystemjb/CouponsystemjbApplication.java
@@ -0,0 +1,15 @@
+package com.projectjb.couponsystemjb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class CouponsystemjbApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(CouponsystemjbApplication.class, args);
+
+	}
+
+}
